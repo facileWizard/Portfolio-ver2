72f632250cdf36da5584cf7a3c8935fdfa2c1510
facileWizard <156981249+facileWizard@users.noreply.github.com>
2024-03-08 22:46:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
-import mobileNav from '../../src/utils/mobile-nav.js';
-import darkMode from '../../src/utils/dark-mode.js';
-import lazyLoading from '../../src/utils/lazy-loading.js';
+import mobileNav from '../src/utils/mobile-nav.js';
+import darkMode from '../src/utils/dark-mode.js';
+import lazyLoading from '../src/utils/lazy-loading.js';
 
 mobileNav();
 darkMode();
