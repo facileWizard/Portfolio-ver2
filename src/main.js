8ba44d310d0b2f3d527d08bf8424ae922b2fb5db
facileWizard <156981249+facileWizard@users.noreply.github.com>
2024-03-08 21:07:25 +0530

    inital commit
    inital commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+
+import mobileNav from '../../src/utils/mobile-nav.js';
+import darkMode from '../../src/utils/dark-mode.js';
+import lazyLoading from '../../src/utils/lazy-loading.js';
+
+mobileNav();
+darkMode();
+lazyLoading();
